a86d1a76ebb1e8ad1e8a3c79ad385082400dc813
Bartosz Jasinski <bjasinski@ittouch.pl>
2022-06-04 10:56:53 +0200

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by ITT on 04/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
