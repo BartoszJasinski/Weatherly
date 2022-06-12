02f16d9173a7823ac749ae5b1f94eae0e90adbdf
Bartosz Jasinski <bjasinski@ittouch.pl>
2022-06-12 10:50:55 +0200

    Before implementing searchbar history commit

--- Weatherly/Network/NetworkRepository.swift
+++ Weatherly/Network/NetworkRepository.swift
@@ -17,11 +17,11 @@ public class NetworkRepository {
 
     private static let BASE_PATH = "https://dataservice.accuweather.com/"
     private static let API_VERSION = "/v1/"
-//    private static let API_KEY = "GIATuLsLemzJHcDfEAawtS33wjCY4IXp"
+    private static let API_KEY = "GIATuLsLemzJHcDfEAawtS33wjCY4IXp"
 //    private static let API_KEY = "KumZMHHA9GKHh8S8N7CqtUvEV8bI5lmg"
 //    private static let API_KEY = "AWXEs1NcvKIXOvzgGIFWWMGAex2qkHnG"
 //    private static let API_KEY = "9GbgJxFuBCIwEIj9K3VSsjrzwd1SVLXX"
-    private static let API_KEY = "AEczK8HNw5a5ACYUvAdxmMNu0AfF4IgI"
+//    private static let API_KEY = "AEczK8HNw5a5ACYUvAdxmMNu0AfF4IgI"
 
     func request(with baseURL: URL) -> URLRequest {
         var request = URLRequest(url: baseURL)
--- Weatherly/Views/WeatherMainView/MainViewController.swift
+++ Weatherly/Views/WeatherMainView/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 import RxSwift
 import RxCocoa
 
